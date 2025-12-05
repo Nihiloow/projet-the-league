7aaf2cb89b5980b1ddbe91a76097f0dfb5a57330
astralamy <amy.lambert@coda-student.school>
2025-12-05 16:03:04 +0100

    remade html files organisation

--- the-league/services/Router.php
+++ the-league/services/Router.php
@@ -1,5 +1,31 @@
 <?php
 
+class Router
+{
+    public function handleRequest() : void
+    {
+        $ctrl = new Controller();
+        $paths = [
+            "show_user" => "show",
+            "create_user" => "create",
+            "check_create_user" => "checkcreate",
+            "update_user" => "update",
+            "check_update_user" => "checkUpdate",
+            "delete_user" => "delete"
+        ];
 
+        if (isset($_GET["route"])){
+            if (isset($paths[$_GET["route"]])){
+                $ctrl->$paths[$_GET["route"]]();
+            }
 
-?>
+            else{
+                $ctrl->list();
+            }
+        }
+
+        else{
+            $ctrl->list();
+        }
+    }
+}
