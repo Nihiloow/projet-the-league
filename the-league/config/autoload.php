3f1f46587d0258b2b2cce135b4472aef0a787c0d
Nihilow <nathan.vincente@coda-student.school>
2025-12-05 15:54:16 +0100

    Réarrangement des partials

--- the-league/config/autoload.php
+++ the-league/config/autoload.php
@@ -1,8 +1,13 @@
 <?php
-require "manager/AbstractManager.php";
 
 require "models/Game.php";
 require "models/Media.php";
 require "models/Player_performance.php";
 require "models/Player.php";
 require "models/Team.php";
+require "manager/AbstractManager.php";
+
+
+
+// ordre :
+// model / (Abstract premier) manager / (Abstract premier) controller / service
