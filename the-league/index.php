dc36b72707658bddb107860f4ebf7aa2c6acd30e
Nihilow <nathan.vincente@coda-student.school>
2025-12-07 13:10:11 +0100

    Ajout des instances des Managers + connexion des données des partials de la page home avec la database

--- the-league/index.php
+++ the-league/index.php
@@ -2,5 +2,11 @@
 
 require "config/autoload.php";
 
+$gameManager = new GameManager() ;
+$mediaManager = new MediaManager() ;
+$playerManager = new PlayerManager() ;
+$player_perfManager = new Player_PerformanceManager() ;
+$teamManager = new TeamManager() ;
+
 $router = new Router();
 $router->handleRequest();
