3e22bd409fc3ddde1284385be8cd2aeef9975e66
sofianecharifi <sofiane.charifialaoui@gmail.com>
2025-12-07 23:51:08 +0100

    Add styling for matchs_section header in CSS

--- the-league/manager/AbstractManager.php
+++ the-league/manager/AbstractManager.php
@@ -12,7 +12,7 @@ abstract class AbstractManager
         $connexionString = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8";
 
         $user = "root";
-        $password = "demopma";  //mdp avant de commit : demopma (QUE POUR SOFIANE)
+        $password = "demopma";
 
         $this->db = new PDO(
             $connexionString,
